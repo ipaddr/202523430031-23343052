6b204ebf5f993b33d1fcd43a829481e111917203
RidhoHamdaniPutra <ridhohamdaniputra@gmail.com>
2026-02-24 22:46:45 +0700

    Auth Service

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,26 @@
+import 'package:belajarflutter/services/auth/auth_provider.dart';
+import 'package:belajarflutter/services/auth/auth_user.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+  const AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) => provider.createUser(email: email, password: password);
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) =>
+      provider.logIn(email: email, password: password);
+
+  @override
+  Future<AuthUser> logOut() => provider.logOut();
+
+  @override
+  Future<AuthUser> sendEmailVerification() => provider.sendEmailVerification();
+}
