2b9ce1baaa0846f4d78b09b58f4f6653251792b6
RidhoHamdaniPutra <ridhohamdaniputra@gmail.com>
2026-03-03 19:54:09 +0700

    Creating New Notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:belajarflutter/constans/routes.dart';
 import 'package:belajarflutter/services/auth/auth_service.dart';
 import 'package:belajarflutter/views/login_view.dart';
+import 'package:belajarflutter/views/notes/new_note_view.dart';
 import 'package:belajarflutter/views/notes_view.dart';
 import 'package:belajarflutter/views/register_view.dart';
 import 'package:belajarflutter/views/verify_email_view.dart';
@@ -25,7 +26,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NotesView(),
+        newNoteRoute: (context) => const NewNoteView(),
       },
     ),
   );
