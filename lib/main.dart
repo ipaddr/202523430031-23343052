7988b28d49b7df4492cc351e1c783d28f3eba203
RidhoHamdaniPutra <ridhohamdaniputra@gmail.com>
2026-03-10 00:10:43 +0700

    Moving to Bloc for Routing and Dialogs

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,6 @@ void main() {
         child: const HomePage(),
       ),
       routes: {
-        loginRoute: (context) => const LoginView(),
-        registerRoute: (context) => const RegisterView(),
-        notesRoute: (context) => const NotesView(),
-        verifyEmailRoute: (context) => const VerifyEmailView(),
         CreateOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
@@ -53,6 +49,8 @@ class HomePage extends StatelessWidget {
           return const VerifyEmailView();
         } else if (state is AuthStateLoggedOut) {
           return const LoginView();
+        } else if (state is AuthStateRegistering) {
+          return const RegisterView();
         } else {
           return Scaffold(body: CircularProgressIndicator());
         }
